4edb45b9ff89b44bd0ef0a7d11b2a51a6bbf2281
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-09-09 19:43:46 -0500

    Added findAll get request to category-routes.js.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,6 +4,26 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    attribute:['id', 'category_name'],
+    include:[
+      {
+        model: Product,
+        attribute:[
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ],
+  })
+  .then(category => res.join(category))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // find all categories
   // be sure to include its associated Products
 });
